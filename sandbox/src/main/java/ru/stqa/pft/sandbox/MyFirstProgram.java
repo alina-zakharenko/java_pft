0b556ebfefff136f860f0108af61609e10d64e05
alina-zakharenko <alinazakharenko@gmail.com>
2021-04-14 21:37:38 +0300

    Homework first test for calculation point's distance

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -9,27 +9,17 @@ public class MyFirstProgram {
 
 
     Square s = new Square(5);
-    //s.l = 5.0;
-    System.out.println("Площадь квадрата со стороной " + s.l + " = " + area(s));
+    System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
 
     Rectangle r = new Rectangle(4, 6);
-    //r.a = 4;
-    // r.b = 6;
-    System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + area(r));
+    System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
 
   }
 
   public static void hello(String text) {
-    //String text = "world";
     System.out.println("Hello, " + text + "!");
 
   }
 
-  public static double area(Square square) {
-    return square.l * square.l;
-  }
 
-  public static double area(Rectangle rectangle) {
-    return rectangle.a * rectangle.b;
-  }
 }
--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -14,6 +14,7 @@ public class Point {
     return d;
   }
 
+
   public static void main(String[] args) {
     Point p1 = new Point(2, 1);
     Point p2 = new Point(6, 4);
