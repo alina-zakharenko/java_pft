2e78ac1e467ac2cad61131c08ee4713cd1be5d78
alina-zakharenko <alinazakharenko@gmail.com>
2021-04-21 22:53:39 +0300

    Code refactor, add test for group deletion

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/NewUserCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/NewUserCreationTest.java
@@ -1,25 +1,8 @@
 package ru.stqa.pft.addressbook;
 
-import java.util.concurrent.TimeUnit;
+import org.testng.annotations.Test;
 
-import org.testng.annotations.*;
-
-import static org.testng.Assert.*;
-
-import org.openqa.selenium.*;
-import org.openqa.selenium.firefox.FirefoxDriver;
-
-public class NewUserCreationTest {
-  private WebDriver wd;
-
-
-  @BeforeMethod(alwaysRun = true) //метод инициализации фикстуры
-  public void setUp() throws Exception {
-    wd = new FirefoxDriver();
-    wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-    wd.get("http://localhost/addressbook/group.php");
-    login("admin", "secret");
-  }
+public class NewUserCreationTest extends TestBase {
 
   @Test
   public void testNewUserCreation() throws Exception {
@@ -28,61 +11,5 @@ public class NewUserCreationTest {
     submitInfo();
     gotoHomePage();
   }
-
-
-  private void login(String username, String password) {
-    wd.findElement(By.name("user")).clear();
-    wd.findElement(By.name("user")).sendKeys(username);
-    wd.findElement(By.name("pass")).click();
-    wd.findElement(By.name("pass")).clear();
-    wd.findElement(By.name("pass")).sendKeys(password);
-    wd.findElement(By.xpath("//input[@value='Login']")).click();
-  }
-
-  private void gotoHomePage() {
-    wd.findElement(By.xpath("//div[@id='header']/a")).click();
-  }
-
-  private void submitInfo() {
-    wd.findElement(By.xpath("(//input[@name='submit'])[2]")).click();
-  }
-
-  private void fillUserInfo(UserData userData) {
-    wd.findElement(By.name("firstname")).click();
-    wd.findElement(By.name("firstname")).clear();
-    wd.findElement(By.name("firstname")).sendKeys(userData.getFirstname());
-    wd.findElement(By.name("lastname")).click();
-    wd.findElement(By.name("lastname")).clear();
-    wd.findElement(By.name("lastname")).sendKeys(userData.getLastname());
-    wd.findElement(By.name("email")).click();
-    wd.findElement(By.name("email")).clear();
-    wd.findElement(By.name("email")).sendKeys(userData.getEmail());
-  }
-
-  private void GotoCreateUserPage() {
-    wd.findElement(By.linkText("add new")).click();
-  }
-
-  @AfterMethod(alwaysRun = true) //метод завершения фикстуры
-  public void tearDown() throws Exception {
-    wd.quit();
-  }
-
-  private boolean isElementPresent(By by) {
-    try {
-      wd.findElement(by);
-      return true;
-    } catch (NoSuchElementException e) {
-      return false;
-    }
-  }
-
-  private boolean isAlertPresent() {
-    try {
-      wd.switchTo().alert();
-      return true;
-    } catch (NoAlertPresentException e) {
-      return false;
-    }
-  }
+  
 }
