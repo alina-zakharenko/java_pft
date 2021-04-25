a9ed5df4c9b6c912840401934710e84e3baa9e90
alina-zakharenko <alinazakharenko@gmail.com>
2021-04-25 13:49:40 +0300

    Lection 3.3

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/TestBase.java
@@ -1,12 +1,13 @@
 package ru.stqa.pft.addressbook.model;
 
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
 
 public class TestBase {
 
-  protected final ApplicationManager applicationManager = new ApplicationManager();
+  protected final ApplicationManager applicationManager = new ApplicationManager(BrowserType.CHROME);
 
   @BeforeMethod(alwaysRun = true) //метод инициализации фикстуры
   public void setUp() throws Exception {
