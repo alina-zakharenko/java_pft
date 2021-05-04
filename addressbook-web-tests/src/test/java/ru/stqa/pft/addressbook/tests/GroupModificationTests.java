f63137342c2d4bf0d9e517c9e4a8c63fb4d59a69
alina-zakharenko <alinazakharenko@gmail.com>
2021-05-04 16:50:07 +0300

    Code change after lection 4.7

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -5,6 +5,7 @@ import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 import ru.stqa.pft.addressbook.model.TestBase;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupModificationTests extends TestBase {
@@ -12,18 +13,23 @@ public class GroupModificationTests extends TestBase {
   public void testGroupModification() {
     applicationManager.getNavigationHelper().gotoGroupPage();
     if (!applicationManager.getGroupHelper().isThereAGroup()) {
-      applicationManager.getGroupHelper().createGroup(new GroupData("test1", null, null));
+      applicationManager.getGroupHelper().createGroup(new GroupData( "test1", null, null));
     }
     List<GroupData> before = applicationManager.getGroupHelper().getGroupList();
     applicationManager.getGroupHelper().selectGroup(before.size() - 1);
     System.out.println("Количество групп до " + before.size());
     applicationManager.getGroupHelper().initGroupModification();
-    applicationManager.getGroupHelper().fillGroupForm(new GroupData("test1", "test2", "test3"));
+    GroupData group = new GroupData(before.get(before.size() - 1).getId(), "test1", "test2", "test3");
+    applicationManager.getGroupHelper().fillGroupForm(group);
     applicationManager.getGroupHelper().submitGroupModification();
     applicationManager.getGroupHelper().returnToGroupPage();
     List<GroupData> after = applicationManager.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size(), "Количество групп до и после не совпадает.");
     System.out.println("Количество групп до " + before.size() + " и после " + after.size());
+
+    before.remove(before.size() - 1);
+    before.add(group);
+    Assert.assertEquals(new HashSet<>(before), new HashSet<>(after));
   }
 
 }
