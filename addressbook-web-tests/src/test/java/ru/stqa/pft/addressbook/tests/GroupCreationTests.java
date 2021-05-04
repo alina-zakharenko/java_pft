7aed25a78a42837d9257c07f0b65b75e02905b18
alina-zakharenko <alinazakharenko@gmail.com>
2021-05-04 17:37:59 +0300

    Code change after lection 4.8

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -6,6 +6,7 @@ import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 import ru.stqa.pft.addressbook.model.TestBase;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupCreationTests extends TestBase {
@@ -16,13 +17,24 @@ public class GroupCreationTests extends TestBase {
     List<GroupData> before = applicationManager.getGroupHelper().getGroupList();
     //int before = applicationManager.getGroupHelper().getGroupCount();
     System.out.println("Количество групп до " + before.size());
-    applicationManager.getGroupHelper().createGroup(new GroupData("test1", "test2", "test3"));
+    GroupData group = new GroupData("test1", "test2", "test3");
+    applicationManager.getGroupHelper().createGroup(group);
     List<GroupData> after = applicationManager.getGroupHelper().getGroupList();
     //int after = applicationManager.getGroupHelper().getGroupCount();
     applicationManager.getNavigationHelper().gotoHomePage();
     Assert.assertEquals(after.size(), before.size() +  1, "Количество групп после не увеличилось.");
     System.out.println("Количество групп после " + after.size());
 
+    before.add(group);
+    int max = 0;
+    for (GroupData g: after ){
+      if (g.getId()>max){
+        max = g.getId();
+      }
+    }
+
+    group.setId(max);
+    Assert.assertEquals(new HashSet<>(before), new HashSet<>(after));
   }
 
 }
