13c058c6cdc0739bd9576daee62102b6e50ea711
alina-zakharenko <alinazakharenko@gmail.com>
2021-06-14 13:37:30 +0300

    Code change after lection 9.2

--- soap-sample/src/test/java/ru/stqa/pft/soap/GeoIpServiceTests.java
+++ soap-sample/src/test/java/ru/stqa/pft/soap/GeoIpServiceTests.java
@@ -12,9 +12,4 @@ public class GeoIpServiceTests {
     String ipLocation = new GeoIPService().getGeoIPServiceSoap12().getIpLocation("93.100.46.23");
     assertEquals(ipLocation, "<GeoIP><Country>RU</Country><State>66</State></GeoIP>");
   }
-
-//
-//  public void myIp() {
-//    String location = new GeoIPService().getGeoIPServiceSoap12().getIpLocation("46.138.4.49");
-//    System.out.println(location);
 }
